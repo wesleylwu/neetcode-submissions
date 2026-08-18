ad58ea1a790ed1525036306105ae537cc395cbee
Wesley Wu <wesleylwu@gmail.com>
2026-08-18 11:29:05 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr[::-1] == newStr
